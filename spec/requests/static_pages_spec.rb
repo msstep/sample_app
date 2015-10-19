83f2721065b043c62e388d76d9ce6d02aff45118
3yek <msstepster@gmail.com>
2015-10-19 16:10:39 +0300

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
@@ -9,10 +11,15 @@ describe "Static pages" do
       expect(page).to have_content('Sample App')
     end
 
-	it "should have the right title" do
-	  visit '/static_pages/home'
-	  expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
-	end  
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("#{base_title}")
+    end  
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title("| Home")
+    end    
 
   end
 
@@ -23,10 +30,10 @@ describe "Static pages" do
       expect(page).to have_content('Help')
     end
 
-	it "should have the right title" do
-	  visit '/static_pages/help'
-	  expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
-	end      
+  it "should have the right title" do
+    visit '/static_pages/help'
+    expect(page).to have_title("#{base_title}  | Help")
+  end      
   end
 
   describe "About page" do
@@ -36,12 +43,26 @@ describe "Static pages" do
       expect(page).to have_content('About Us')
     end
 
-	it "should have the right title" do
-	  visit '/static_pages/about'
-	  expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
-	end  
+  it "should have the right title" do
+    visit '/static_pages/about'
+    expect(page).to have_title("#{base_title}  | About")
+  end  
 
   end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+  it "should have the right title" do
+    visit '/static_pages/contact'
+    expect(page).to have_title("#{base_title}  | Contact")
+  end  
+
+  end  
 
 
 end
