3bda94ba7595ceaf8e39248a999eb2082945d6a3
3yek <msstepster@gmail.com>
2015-11-05 14:33:41 +0300

    Finish body of chapter 8 with theme sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   #get 'users/new'
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   #get 'static_pages/home'
   #match '/', to: 'static_pages#home', via: 'get'  это не работает
   root  'static_pages#home'
@@ -12,7 +13,8 @@ Rails.application.routes.draw do
   match '/contact', to: 'static_pages#contact', via: 'get'    
 
   match '/signup', to: 'users#new', via: 'get'      
-
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
