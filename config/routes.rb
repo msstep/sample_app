f695bd8fe374afe36e6060d1543c4ce4f07e32f8
msstep <msstep_box@inbox.ru>
2015-11-24 13:52:47 +0300

    ewe ne conec 11 glavi

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   #get 'users/new'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
+  
   #get 'static_pages/home'
   #match '/', to: 'static_pages#home', via: 'get'  это не работает
   root  'static_pages#home'
